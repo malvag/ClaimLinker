f7a7cfb5fc2da539befb962f8eed50c5e230fa1e
Pavlos Fafalios <fafalios.pavlos@gmail.com>
2021-02-24 13:41:06 +0200

    Update Claim.java

--- ElasticSearch_Tools/src/main/java/csd/claimlinker/model/Claim.java
+++ ElasticSearch_Tools/src/main/java/csd/claimlinker/model/Claim.java
@@ -10,87 +10,97 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Claim implements Comparable<Claim> {
-	Map<String, Object> objectMap;
-	private CoreDocument doc;
 
-	Claim() {
-	}
+    Map<String, Object> objectMap;
+    private CoreDocument doc;
 
-	public Claim(JsonObject claim_obj) {
-		objectMap = new HashMap<>();
-		this.objectMap.put("claimReview_claimReviewed", claim_obj.getAsJsonObject("_source").get("claimReview_claimReviewed").getAsString());
-		this.objectMap.put("_score", claim_obj.get("_score").getAsDouble());
-		this.objectMap.put("extra_title", claim_obj.getAsJsonObject("_source").get("extra_title").getAsString());
-		this.objectMap.put("rating_alternateName", claim_obj.getAsJsonObject("_source").get("rating_alternateName").getAsString());
-		this.objectMap.put("creativeWork_author_name", claim_obj.getAsJsonObject("_source").get("creativeWork_author_name").getAsString());
-		this.objectMap.put("claimReview_url", claim_obj.getAsJsonObject("_source").get("claimReview_url").getAsString());
+    Claim() {
+    }
 
-	}
+    public Claim(JsonObject claim_obj) {
+        objectMap = new HashMap<>();
+        this.objectMap.put("claimReview_claimReviewed", claim_obj.getAsJsonObject("_source").get("claimReview_claimReviewed").getAsString());
+        this.objectMap.put("_score", claim_obj.get("_score").getAsDouble());
+        this.objectMap.put("extra_title", claim_obj.getAsJsonObject("_source").get("extra_title").getAsString());
+        this.objectMap.put("rating_alternateName", claim_obj.getAsJsonObject("_source").get("rating_alternateName").getAsString());
+        this.objectMap.put("creativeWork_author_name", claim_obj.getAsJsonObject("_source").get("creativeWork_author_name").getAsString());
+        this.objectMap.put("claimReview_url", claim_obj.getAsJsonObject("_source").get("claimReview_url").getAsString());
+        this.objectMap.put("claim_uri", claim_obj.getAsJsonObject("_source").get("claim_uri").getAsString());
+
+    }
 
 //	public Map<String, Object> getObjectMap() {
 //		return objectMap;
 //	}
+    public Claim(Map<String, Object> in) {
+        this.objectMap = in;
+    }
 
-	public Claim(Map<String, Object> in) {
-		this.objectMap = in;
-	}
+    public void setDoc(CoreDocument doc) {
+        this.doc = doc;
+    }
 
-	public void setDoc(CoreDocument doc) {
-		this.doc = doc;
-	}
+    public void setNLPScore(double nlpScore) {
+        this.objectMap.put("nlp_score", nlpScore);
+    }
 
-	public void setNLPScore(double nlpScore) {
-		this.objectMap.put("nlp_score", nlpScore);
-	}
+    public double getElasticScore() {
+        return (double) this.objectMap.get("_score");
+    }
 
-	public double getElasticScore() {
-		return (double) this.objectMap.get("_score");
-	}
+    public String getReviewedBody() {
+        return (String) this.objectMap.get("claimReview_claimReviewed");
+    }
 
-	public String getReviewedBody() {
-		return (String) this.objectMap.get("claimReview_claimReviewed");
-	}
+    public String getRatingName() {
+        return (String) this.objectMap.get("rating_alternateName");
+    }
 
-	public String getRatingName() {
-		return (String) this.objectMap.get("rating_alternateName");
-	}
+    public Double getNLPScore() {
+        return (Double) this.objectMap.get("nlp_score");
+    }
 
-	public Double getNLPScore() {
-		return (Double) this.objectMap.get("nlp_score");
-	}
+    public String getclaimReviewedURL() {
+        return (String) this.objectMap.get("claimReview_url");
+    }
 
-	public String getclaimReviewedURL() {
-		return (String) this.objectMap.get("claimReview_url");
-	}
+    public String getExtraTitle() {
+        return (String) this.objectMap.get("extra_title");
+    }
 
-	public String getExtraTitle() {
-		return (String) this.objectMap.get("extra_title");
-	}
+    public String getClaimURI() {
+        return (String) this.objectMap.get("claim_uri");
+    }
 
-	public String getAuthorName() {
-		String auth_name = (String) this.objectMap.get("creativeWork_author_name");
-		return auth_name == null || auth_name.isEmpty() ? "" : auth_name;
-	}
+    public String getAuthorName() {
+        String auth_name = (String) this.objectMap.get("creativeWork_author_name");
+        return auth_name == null || auth_name.isEmpty() ? "" : auth_name;
+    }
 
-	public String toJson() throws JsonProcessingException {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.enable(SerializationFeature.INDENT_OUTPUT);
-		return mapper.writeValueAsString(this.objectMap);
-	}
+    public String toJson() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return mapper.writeValueAsString(this.objectMap);
+    }
 
-	@Override
-	public String toString() {
-		try {
-			return this.toJson();
-		} catch (JsonProcessingException e) {
-			return null;
-		}
-	}
+    @Override
+    public String toString() {
+        try {
+            return this.toJson();
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
 
-	@Override
-	public int compareTo(Claim o) {
-		if (this.getNLPScore() == null || o.getNLPScore() == null)
-			return Double.compare(this.getElasticScore(), o.getElasticScore());
-		return Double.compare(this.getNLPScore() + this.getElasticScore(), o.getNLPScore() + o.getElasticScore());
-	}
+    @Override
+    public int compareTo(Claim o) {
+
+        if (this.getNLPScore() == null || o.getNLPScore() == null) {
+            return Double.compare(this.getElasticScore(), o.getElasticScore());
+        }
+        System.out.println("Final Score ("+this.getReviewedBody()+"): " + ((this.getNLPScore() * 100.0) + this.getElasticScore()));
+        System.out.println("Final Score ("+o.getReviewedBody()+"): " + ((o.getNLPScore() * 100.0) + o.getElasticScore()));
+        System.out.println("------------");
+        return Double.compare((this.getNLPScore() * 100.0) + this.getElasticScore(), (o.getNLPScore() * 100.0) + o.getElasticScore());
+    }
 }
